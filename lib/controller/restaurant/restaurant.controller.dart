e0752b3bb67c2680295aa52294ab64005284f22f
Nasir74 <nasir@royex.net>
2022-05-24 11:16:08 +0600

    restaurants on refresh

--- lib/controller/restaurant/restaurant.controller.dart
+++ lib/controller/restaurant/restaurant.controller.dart
@@ -1,14 +1,6 @@
-import 'package:Softcent/main.dart';
 import 'package:Softcent/model/model.dart' as m;
 import 'package:get/get.dart';
 
 class RestaurantController extends GetxController {
   final m.Restaurant restaurant = Get.arguments as m.Restaurant;
-
-  @override
-  onInit(){
-    super.onInit();
-    dPrint('restaurant received: =================================================');
-    dPrint(restaurant.toString());
-  }
 }
